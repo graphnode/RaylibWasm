0faecedeb0ab79e90edd053a2b6c6e31d5fd7386
Diogo Gomes <dgomes@graphnode.com>
2025-05-22 22:30:50 +0100

    Updated raylib-cs, made canvas resizable and gave priority to http instead of https.

--- RaylibWasm/Application.cs
+++ RaylibWasm/Application.cs
@@ -1,21 +1,26 @@
-﻿using System.Runtime.InteropServices.JavaScript;
+﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices.JavaScript;
 using Raylib_cs;
 
 namespace RaylibWasm
 {
     public partial class Application
     {
-        private static Texture2D logo;
+        private static Texture2D _logo;
+        private static Vector2 _position = Vector2.Zero;
+        private static Vector2 _velocity = new(60, 60);
         
         /// <summary>
         /// Application entry point
         /// </summary>
         public static void Main()
         {
+            Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
             Raylib.InitWindow(512, 512, "RaylibWasm");
-            Raylib.SetTargetFPS(60);
+            //Raylib.SetTargetFPS(60);
             
-            logo = Raylib.LoadTexture("Resources/raylib_logo.png");
+            _logo = Raylib.LoadTexture("Resources/raylib_logo.png");
         }
 
         /// <summary>
@@ -24,14 +29,26 @@ namespace RaylibWasm
         [JSExport]
         public static void UpdateFrame()
         {
+            var dt = Raylib.GetFrameTime();
+            
+            _position += _velocity * dt;
+            
+            if (_position.X < 0 || _position.X > Raylib.GetScreenWidth() - _logo.Width)
+                _velocity.X *= -1;
+            if (_position.Y < 0 || _position.Y > Raylib.GetScreenHeight() - _logo.Height)
+                _velocity.Y *= -1;
+            
+            _position.X = Math.Clamp(_position.X, 0, Raylib.GetScreenWidth() - _logo.Width);
+            _position.Y = Math.Clamp(_position.Y, 0, Raylib.GetScreenHeight() - _logo.Height);
+            
             Raylib.BeginDrawing();
 
-            Raylib.ClearBackground(Color.White);
+            Raylib.ClearBackground(Color.RayWhite);
 
             Raylib.DrawFPS(4, 4);
             Raylib.DrawText("All systems operational!", 4, 32, 20, Color.Maroon);
             
-            Raylib.DrawTexture(logo, 4, 64, Color.White);
+            Raylib.DrawTextureV(_logo, _position, Color.White);
 
             Raylib.EndDrawing();
         }

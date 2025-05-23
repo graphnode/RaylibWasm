df49a9be3a143183f70d98954a096d8c4fda5b28
Diogo Gomes <dgomes@graphnode.com>
2025-05-23 20:22:19 +0100

    Replaced example with raymarching. Updated raylib web library to 5.5 to fix some bugs.

--- RaylibWasm/Application.cs
+++ RaylibWasm/Application.cs
@@ -11,10 +11,15 @@ namespace RaylibWasm
 #endif
     public static partial class Application
     {
-        private static Texture2D _logo;
-        private static Vector2 _position = Vector2.Zero;
-        private static Vector2 _velocity = new(60, 60);
-        
+        private static float _runTime;
+        private static Camera3D camera;
+        private static float[] resolution;
+        private static Shader shader;
+        private static int locViewEye;
+        private static int locViewCenter;
+        private static int locRunTime;
+        private static int locResolution;
+
         public static void Main()
         {
             Initialize();
@@ -25,6 +30,8 @@ namespace RaylibWasm
                 Update(Raylib.GetFrameTime());
                 Draw();
             }
+            
+            Terminate();
 #endif
         }
         
@@ -39,38 +46,96 @@ namespace RaylibWasm
 
         private static void Initialize()
         {
-            Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
-            Raylib.InitWindow(1024, 768, "RaylibWasm");
-            //Raylib.SetTargetFPS(60);
+            const int screenWidth = 800;
+            const int screenHeight = 450;
             
-            _logo = Raylib.LoadTexture("Resources/raylib_logo.png");
+#if !BROWSER
+            var glslVersion = 330;
+#else
+            var glslVersion = 100;
+#endif
+
+            Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
+            Raylib.InitWindow(screenWidth, screenHeight, "raylib [shaders] example - raymarching shapes");
+
+            // Set up a 3D camera
+            camera = new Camera3D
+            {
+                Position = new Vector3(2.5f, 2.5f, 3.0f),
+                Target   = new Vector3(0.0f, 0.0f, 0.7f),
+                Up       = new Vector3(0.0f, 1.0f, 0.0f),
+                FovY     = 65.0f,
+                Projection = CameraProjection.Perspective
+            };
+
+            // Load fragment shader only (0 tells Raylib to use its default vertex shader)
+            var fragPath = $"Resources/shaders/glsl{glslVersion}/raymarching.fs";
+            shader = Raylib.LoadShader(null, fragPath);
+
+            // Get uniform locations
+            locViewEye = Raylib.GetShaderLocation(shader, "viewEye");
+            locViewCenter = Raylib.GetShaderLocation(shader, "viewCenter");
+            locRunTime = Raylib.GetShaderLocation(shader, "runTime");
+            locResolution = Raylib.GetShaderLocation(shader, "resolution");
+
+            // Set resolution once
+            resolution = [screenWidth, screenHeight];
+            Raylib.SetShaderValue(shader, locResolution, resolution, ShaderUniformDataType.Vec2);
+            
+            _runTime = 0f;
         }
 
         private static void Update(float dt)
         {
-            _position += _velocity * dt;
+            if (Raylib.IsMouseButtonPressed(MouseButton.Left))
+            {
+                Raylib.DisableCursor();
+            }
             
-            if (_position.X < 0 || _position.X > Raylib.GetScreenWidth() - _logo.Width)
-                _velocity.X *= -1;
-            if (_position.Y < 0 || _position.Y > Raylib.GetScreenHeight() - _logo.Height)
-                _velocity.Y *= -1;
+            // Update camera (FPS style)
+            Raylib.UpdateCamera(ref camera, CameraMode.FirstPerson);
             
-            _position.X = Math.Clamp(_position.X, 0, Raylib.GetScreenWidth() - _logo.Width);
-            _position.Y = Math.Clamp(_position.Y, 0, Raylib.GetScreenHeight() - _logo.Height);
+            // Feed shader with camera and time
+            float[] camPos    = [camera.Position.X, camera.Position.Y, camera.Position.Z];
+            float[] camTarget = [camera.Target.X,   camera.Target.Y,   camera.Target.Z];
+
+            _runTime += Raylib.GetFrameTime();
+            Raylib.SetShaderValue(shader, locViewEye,    camPos,    ShaderUniformDataType.Vec3);
+            Raylib.SetShaderValue(shader, locViewCenter, camTarget, ShaderUniformDataType.Vec3);
+            Raylib.SetShaderValue(shader, locRunTime,    _runTime,   ShaderUniformDataType.Float);
+
+            // Handle window resize
+            if (Raylib.IsWindowResized())
+            {
+                resolution[0] = Raylib.GetScreenWidth();
+                resolution[1] = Raylib.GetScreenHeight();
+                Raylib.SetShaderValue(shader, locResolution, resolution, ShaderUniformDataType.Vec2);
+            }
+
         }
 
         private static void Draw()
         {
             Raylib.BeginDrawing();
-
             Raylib.ClearBackground(Color.RayWhite);
 
-            Raylib.DrawFPS(4, 4);
-            Raylib.DrawText("All systems operational!", 4, 32, 20, Color.Maroon);
+            Raylib.BeginShaderMode(shader);
+            Raylib.DrawRectangle(0, 0, Raylib.GetScreenWidth(), Raylib.GetScreenHeight(), Color.White);
+            Raylib.EndShaderMode();
             
-            Raylib.DrawTextureV(_logo, _position, Color.White);
+            Raylib.DrawText("(c) Raymarching shader by Iñigo Quilez. MIT License.",
+                Raylib.GetScreenWidth() - 280, Raylib.GetScreenHeight() - 20,
+                10, Color.Black);
 
+            Raylib.DrawFPS(10, 10);
+            
             Raylib.EndDrawing();
+        }
+
+        private static void Terminate()
+        {
+            Raylib.UnloadShader(shader);
+            Raylib.CloseWindow();
         }
     }
 }
